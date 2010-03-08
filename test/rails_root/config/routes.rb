5082466a472a45c8a84d146ecb516f97569652b7
Brian Maddy <bmaddy@bender.local>
2010-03-07 19:16:52 -0600

    removed a bunch of deprecation in routes file

--- test/rails_root/config/routes.rb
+++ test/rails_root/config/routes.rb
@@ -1,4 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
+# try the rails 3 way first, otherwise, fall back to the rails 2 way
+(Rails.application.routes or ActionController::Routing::Routes).draw do |map|
   resources :users do
     resources :posts
   end
